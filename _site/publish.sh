0cf54ba3cc59433a75809c01ae1036ef6e7d98c3
wodeni <wn2155@columbia.edu>
2018-01-18 01:44:50 -0500

    uploading to Columbia server

--- _site/publish.sh
+++ _site/publish.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-bundle exec jekyll build --config "_config-columbia.yml"
+JEKYLL_ENV=production bundle exec jekyll build --config "_config-columbia.yml"
 git add -A
 git ci -m "uploading to Columbia server"
 git push
