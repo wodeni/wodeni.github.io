bc9957a00711c5fe0bbf8aa46d48867435d7d2a6
wodeni <wn2155@columbia.edu>
2018-03-14 03:29:55 -0400

    uploading to Columbia server

--- publish.sh
+++ publish.sh
@@ -4,5 +4,5 @@ JEKYLL_ENV=production bundle exec jekyll build --config "_config-columbia.yml"
 git add -A
 git ci -m "uploading to Columbia server"
 git push
-rsync -v -r -e ssh ~/dev/nimo-site/_site/ cunix:~/public_html
-# ssh cunix "cd ~; sh publish.sh"
+# rsync -v -r -e ssh ~/dev/nimo-site/_site/ cunix:~/public_html
+ssh cunix "cd ~; sh publish.sh"
