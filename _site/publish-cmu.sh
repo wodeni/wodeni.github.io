ecc415bb5a411cdbdd0fed946b2ef77a5e8f4b41
wodeni <wn2155@columbia.edu>
2018-07-15 02:39:50 -0400

    added cmu email and removed redundant _site tracking info

--- _site/publish-cmu.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-JEKYLL_ENV=production bundle exec jekyll build --config "_config-cmu.yml"
-rsync -v -r -e ssh ~/dev/nimo-site/_site/ cmu:~/www
--- publish.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-JEKYLL_ENV=production bundle exec jekyll build --config "_config-cmu.yml"
-git add -A
-git ci -m "uploading to CMU server"
-git push
-ssh cunix "cd ~; sh publish.sh"
