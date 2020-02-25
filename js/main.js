66152a731ac271dd7e4918153a15f9bd0edb245b
wodeni <wn2155@columbia.edu>
2020-02-25 09:44:26 -0500

    tweaking js

--- js/main.js
+++ js/main.js
@@ -15,15 +15,15 @@ $(document).ready(function () {
   })
 
   $('a.about-button').click(function (e) {
-      if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-      currentWidth = $('.panel-cover').width()
-      if (currentWidth < 960) {
-        $('.panel-cover').addClass('panel-cover--collapsed')
-        $('.content-wrapper').addClass('animated slideInRight')
-      } else {
-        $('.panel-cover').css('max-width', currentWidth)
-        $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
-      }
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    currentWidth = $('.panel-cover').width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper').addClass('animated slideInRight')
+    } else {
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    }
   })
 
   if (window.location.hash && (window.location.hash == '#blog' || window.location.hash == '#about')) {
@@ -33,7 +33,7 @@ $(document).ready(function () {
   if (window.location.pathname !== '{{ site.baseurl }}//'
       && window.location.pathname !== '{{ site.baseurl }}/'
       && window.location.pathname !== '{{ site.baseurl }}'
-      && window.location.pathname !== '{{ site.baseurl }}index.html') {
+      && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
