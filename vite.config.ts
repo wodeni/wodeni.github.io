362f3a321356f3e8dd3ec7b2d5cafa803aa75852
wodeni <wn2155@columbia.edu>
2023-06-05 03:58:55 -0400

    chore: assets without hash

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,10 @@ export default defineConfig({
   },
   build: {
     target: "esnext",
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
   },
 });
