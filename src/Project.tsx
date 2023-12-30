d72d17efb13b259ddf47dd22466e992b2f6e6944
Wode "Nimo" Ni <wn2155@columbia.edu>
2023-12-30 16:16:11 -0500

    feat: add dark mode support to the site (#18)
    * feat: dark mode prototype
    
    * fix: small layout issues

--- src/Project.tsx
+++ src/Project.tsx
@@ -1,10 +1,11 @@
-import theme from "./theme";
 export default ({
   name,
   desc,
   link,
   logo,
+  dark,
 }: {
+  dark?: boolean;
   name: string;
   desc: string;
   link: string;
@@ -13,17 +14,19 @@ export default ({
   return (
     <a href={link}>
       <div
-        className="rounded drop-shadow-md hover:drop-shadow-xl w-full h-36 lg:h-48 bg-right"
+        className="rounded drop-shadow-md hover:drop-shadow-xl w-full h-36 lg:h-48 bg-right bg-white dark:shadow-none dark:bg-zinc-700 dark:rounded-lg dark:border-primary"
         style={{
-          backgroundImage: `
-            linear-gradient(to right, rgba(255, 255, 255, 1), 80%, rgba(255, 255, 255, 0)), 
+          backgroundImage: dark
+            ? ``
+            : `
+            linear-gradient(to right, rgba(255, 255, 255, 1), 80%, rgba(255, 255, 255, 0)),
             url(${logo})`,
           backgroundPosition: "left",
           backgroundSize: "250% 200%",
           backgroundPositionX: "20%",
         }}
       >
-        <div className="text-3xl font-medium w-7/10 px-6 lg:py-6 text-primary/70">
+        <div className="text-3xl font-medium w-7/10 px-6 pt-4 lg:py-6 text-primary/70 dark:text-primary">
           {name}
         </div>
         <div className="text-lg font-extralight w-7/10 px-6 py-4 lg:py-6">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       scale: {
         400: "40",
       },
-    },
-    colors: {
-      primary: "#C559F4",
-      // primary: "#2C1EF1",
-      icon: "#999",
-      icontext: "#1E1E1E",
+      colors: {
+        primary: "#C559F4",
+        // primary: "#2C1EF1",
+        icon: "#999",
+        "icon-dark": "#bbb",
+        icontext: "#1E1E1E",
+        "icontext-dark": "#fff",
+      },
     },
   },
   plugins: [],
